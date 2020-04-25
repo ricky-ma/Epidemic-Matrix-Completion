64e7cffe2832fc4af8241b919220926f4082df5e
rickyma <mr.rickyma@gmail.com>
2020-04-24 21:21:32 -0700

    Finished preprocessing for covid data

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 import pandas as pd
 
 
-
-
-
-
-
-def days_since_first_case():
-    df = pd.read_csv('data/time_series_covid19_confirmed_global.csv', header=0)
-
-
-
-def group_by_country(df):
-    print(df.shape)
-    df.drop('Province/State', axis=1, inplace=True)
-    print(df.shape)
-    for date in df.columns[3:]:
-        df[date] = df.groupby(['Country/Region'])[date].transform('sum')
-    df.drop_duplicates(subset=['Country/Region'], keep='first', inplace=True)
+def load_covid_data(df):
+    df.drop(columns=['Lat', 'Long', 'Province/State'], inplace=True)
+    for date in df.columns[1:]:
+        df[date] = df.groupby([df.columns[0]])[date].transform('sum')
+    df.drop_duplicates(subset=[df.columns[0]], keep='first', inplace=True)
+    df['Days since first'] = 0
+    for index, country in df.iterrows():
+        for date in country[1:]:
+            if date > 0:
+                df.at[index, 'Days since first'] += 1
     return df
 
 
-
 if __name__ == '__main__':
-    df = pd.read_csv('data/time_series_covid19_confirmed_global.csv', header=0)
-    df_country = group_by_country(df)
+
+    df_confirmed_global = pd.read_csv('data/time_series_covid19_confirmed_global.csv', header=0)
+    df_confirmed_global = load_covid_data(df_confirmed_global)
+    df_confirmed_global = df_confirmed_global.sort_values(by=['4/23/20', 'Days since first'], ascending=False)
+
+    df_deaths_global = pd.read_csv('data/time_series_covid19_deaths_global.csv', header=0)
+    df_deaths_global = load_covid_data(df_deaths_global)
+    df_deaths_global = df_deaths_global.sort_values(by=['4/23/20', 'Days since first'], ascending=False)
+
+    print(df_confirmed_global.head(10))
+    print(df_deaths_global.head(10))
+
